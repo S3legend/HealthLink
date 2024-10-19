155b0359476666fbed2a90696c84eec50463def5
Your Name <you@example.com>
2024-10-19 13:29:18 +0100

    cmplx mdp

--- Ahthentification/doctor_signup/src/main/java/com/HealthLink/doctor_signup/DTO/DoctorRegistrationDTO.java
+++ Ahthentification/doctor_signup/src/main/java/com/HealthLink/doctor_signup/DTO/DoctorRegistrationDTO.java
@@ -3,6 +3,7 @@ package com.HealthLink.doctor_signup.DTO;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,20 +15,42 @@ public class DoctorRegistrationDTO {
     @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @Email(message = "Email should be valid")
-    @NotBlank(message = "Email is required")
+    @Email(message = "L'adresse email doit être valide.")
+    @Pattern(regexp = "^[\\w-\\.]+@[\\w-]+\\.[a-zA-Z]{2,4}$", message = "L'adresse email doit contenir un '@' et un '.'")
+    @NotBlank
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$",
-            message = "Password must be at least 8 characters long, contain at least one digit, one lowercase letter, one uppercase letter, and one special character.")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).*$",
+            message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and no whitespace")
     private String password;
 
+    @NotBlank
     private String city;
+
+    @NotBlank
     private String country;
+
+    @NotBlank
     private String specialty;
+
+    @NotBlank
     private String address;
+
+    @NotBlank
     private String phoneNumber;
+
     private String officePhoneNumber;
+
     private boolean isCertified;
+
+
+
+
+
+
+
+
 }
+
