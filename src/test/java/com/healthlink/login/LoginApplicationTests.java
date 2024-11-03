2c0a676073ee64391585d5f5dde4c3756098aa44
YASMINE AZROUF <azroufyasmine02@gmail.com>
2024-11-03 22:53:14 +0100

    Login commit: Spring Boot Healthlink

---
+++ src/test/java/com/healthlink/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthlink.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
