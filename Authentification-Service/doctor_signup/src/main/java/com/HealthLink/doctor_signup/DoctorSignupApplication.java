385a0cc84ddbd01a194f19fe41a5d9dc536b3cb5
Your Name <you@example.com>
2024-12-05 20:49:23 +0100

    auth-back-modify

---
+++ Authentification-Service/doctor_signup/src/main/java/com/HealthLink/doctor_signup/DoctorSignupApplication.java
@@ -0,0 +1,13 @@
+package com.HealthLink.doctor_signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorSignupApplication.class, args);
+	}
+
+}
