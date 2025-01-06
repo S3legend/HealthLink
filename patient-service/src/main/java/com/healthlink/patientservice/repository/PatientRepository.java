a1cf8311c1e9f25c1c1f81c66c4ae4552f6767cf
Yasmine AZROUF <153874621+yasmina0410@users.noreply.github.com>
2025-01-06 21:53:50 +0100

    Delete patient-service directory

--- patient-service/src/main/java/com/healthlink/patientservice/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.healthlink.patientservice.repository;
-
-import com.healthlink.patientservice.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
